58ed82d10054f7712e8add5d5003f2dc9130e36e
Messquerade <annac.klingberg@gmail.com>
2021-08-27 09:33:22 -0700

    add methods to add toppings and size to pizza.

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,46 @@
 // Business Logic
-function Pizza() {
-  this.size = {};
-  this.toppings = {};
+function Parlor() {
+  this.pizzas = {};
+  this.currentId = 0;
 }
 
-function Size(diameter, cost) {
+Parlor.prototype.addPizza = function(pizza) {
+  this.currentId++;
+  this.pizzas[this.currentId] = pizza;
+}
+
+function Pizza() {
+  this.sizes = {};
+  this.toppings = {};
+  this.currentId = 0;
+}
+
+Pizza.prototype.addSize = function(size) {
+  this.currentId++;
+  this.sizes[this.currentId] = size;
+}
+
+Pizza.prototype.addTopping = function(topping) {
+  this.currentId++;
+  this.toppings[this.currentId] = topping;
+}
+
+
+function Size(name, diameter, cost) {
+  this.name = name;
   this.diameter = diameter;
   this.cost = cost;
 }
 
 function Topping(name, cost) {
-  this.diameter = diameter;
+  this.name = name;
   this.cost = cost;
 }
+
+const parlor = new Parlor;
+let newpizza = new Pizza;
+parlor.addPizza(newpizza);
+let cheese = new Topping("cheese", 1);
+let small = new Size("small", 6, 10);
+newpizza.addTopping(cheese);
+newpizza.addSize(small);
